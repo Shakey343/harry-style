76ce04975f3c6c4962ec2c26fa18d807f5f6dc0a
Jake Pople <jakepople@hotmail.co.uk>
2024-07-12 23:59:50 +0100

    added <> </> to home.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,8 @@ const Navbar = () => {
     <nav className="bg-hsBlack w-full sticky top-0 flex justify-between text-hsBeige py-2 px-4 items-center shadow-xl">
       <NavLink to="/" className="font-bold text-4xl mx-4 my-2 hover:text-hsRed">Harry Style</NavLink>
       <div>
-        <a src="/#about" className="px-3 hover:text-hsRed border-solid border-r-2 border-hsBeige">About</a>
+        <NavLink src="/#about" className="px-3 hover:text-hsRed border-solid border-r-2 border-hsBeige">About</NavLink>
         <NavLink to="/projects" className="px-3 hover:text-hsRed">Projects</NavLink>
-        {/* comment */}
       </div>
     </nav>
   );
