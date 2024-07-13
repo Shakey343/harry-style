1b389a94333463583e8b84950e48a86bfa0552e9
Jake Pople <jakepople@hotmail.co.uk>
2024-07-13 01:44:02 +0100

    added content to home page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
       className="py-[25vh] bg-cover bg-center"
       style={{
         backgroundImage:
-          "linear-gradient(to right, rgba(0,0,0,0.6), rgba(0,0,0,0)), url(http://localhost:3000/assets/at_the_piano-927c6b07024ae358bb711c8f859fccff7657aefd1790a0db7530364d65168056.jpg)",
+          "linear-gradient(to right, rgba(0,0,0,0.6), rgba(0,0,0,0)), url(https://www.harrystyle.co.uk/assets/at_the_piano-927c6b07024ae358bb711c8f859fccff7657aefd1790a0db7530364d65168056.jpg)",
       }}
     >
       <div className="container mx-auto drop-shadow-2xl text-3xl font-medium relative text-hsBeige">
